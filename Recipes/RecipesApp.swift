21fe8a81ea78779882ee4817f6b1aa7264fbb1ba
Nick <46145522+npegnatz@users.noreply.github.com>
2024-03-27 12:53:48 -0400

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Secondary on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
