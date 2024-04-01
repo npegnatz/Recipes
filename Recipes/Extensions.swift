1c6ea6d62da30c8bf5520532d75edcccc10a03e9
Nick <46145522+npegnatz@users.noreply.github.com>
2024-03-31 23:22:16 -0400

    Extensions formatting

--- Recipes/Extensions.swift
+++ Recipes/Extensions.swift
@@ -8,12 +8,13 @@
 import Foundation
 
 extension String {
+  /** Returns the number value at the end of a string */
   func integerSuffix() -> Int? {
-      let nonDigitCharacterSet = CharacterSet.decimalDigits.inverted
-      let components = self.components(separatedBy: nonDigitCharacterSet)
-      if let lastComponent = components.last, let number = Int(lastComponent) {
-          return number
-      }
-      return nil
+    let nonDigitCharacterSet = CharacterSet.decimalDigits.inverted
+    let components = self.components(separatedBy: nonDigitCharacterSet)
+    if let lastComponent = components.last, let number = Int(lastComponent) {
+      return number
+    }
+    return nil
   }
 }
